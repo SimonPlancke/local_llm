d3e44cac5232e4a642a4cb065cd399afe16dddd1
Simon-Plancke <simon.plancke@keyrus.com>
2025-01-12 15:23:57 +0100

    cleanup

--- debug.py
+++
@@ -1,8 +0,0 @@
-content = '/tree/thisworks?'
-
-if content.startswith(('/downloads', '/tree', '/uptodate')):
-    pdf_url = 'https://sci-hub.se' + content
-else:
-    pdf_url = 'https:/' + content
-
-print(pdf_url)
